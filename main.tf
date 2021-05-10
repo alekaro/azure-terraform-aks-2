1d71ee8ce3f4a67af65deaa36c5b1b9541619159
alekaro <olehsander.karonski@gmail.com>
2021-05-10 20:03:57 +0200

    Some cleaning from main file

--- main.tf
+++ main.tf
@@ -7,14 +7,9 @@ module "asa" {
 }
 
 module "aks" {
-    source                  = "./AKS-COPY"
+    source                  = "./AKS-COPY_2"
     storage_account_id      = module.asa.storage_account_id
     container_registry_id   = module.asa.container_registry_id
-    # storage_account_name    = module.asa.storage_account_name
-    # storage_container_name  = module.asa.storage_container_name
-    # container_registry_name = module.asa.container_registry_name
-    # storage_rg_name         = module.asa.resource_group_name
-    # storage_location        = module.asa.location
 
     depends_on              = [module.asa]
 }
